4d63410644a0d33802be78be55c0ea0c3ce3d62f
Jin Kwon <onacit@gmail.com>
2016-12-23 20:30:52 +0900

    updated

---
+++ main_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+uuid=`uuidgen`
+echo uuid: $uuid
+encoded=$(mvn --quiet exec:java -Dexec.mainClass=com.github.jinahya.codec.IdEncoder -Dexec.args="$uuid")
+echo encoded: $encoded
+decoded=$(mvn --quiet exec:java -Dexec.mainClass=com.github.jinahya.codec.IdDecoder -Dexec.args="$encoded")
+echo decoded: $decoded
+
+long=`od -N 8 -t dL -An /dev/urandom | tr -d " "`
+echo long: $long
+encoded=$(mvn --quiet exec:java -Dexec.mainClass=com.github.jinahya.codec.IdEncoder -Dexec.args="$long")
+echo encoded: $encoded
+decoded=$(mvn --quiet exec:java -Dexec.mainClass=com.github.jinahya.codec.IdDecoder -Dexec.args="$encoded")
+echo decoded: $decoded
+
