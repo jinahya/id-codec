39f850739d3c1c30477319665d819f3ca551a2bd
Jin Kwon <onacit@users.noreply.github.com>
2020-05-30 19:51:29 +0900

    wip

--- src/test/java/com/github/jinahya/codec/CouponNumber.java
+++ src/test/java/com/github/jinahya/codec/CouponNumber.java
@@ -2,14 +2,10 @@ package com.github.jinahya.codec;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.function.BiFunction;
-
 import static java.lang.Character.MAX_RADIX;
-import static java.lang.Character.getNumericValue;
 import static java.lang.Integer.parseInt;
-import static java.lang.String.format;
+import static java.lang.Long.parseLong;
 import static java.util.Objects.requireNonNull;
-import static java.util.concurrent.ThreadLocalRandom.current;
 
 @Slf4j
 class CouponNumber {
@@ -20,59 +16,75 @@ class CouponNumber {
 
     private static final int L_MAX_VALUE_DIGITS = Long.toString(Long.MAX_VALUE).length();
 
-    static String encode16(final int decoded) {
+    static String encodeInt4(int decoded) {
         assert decoded >= 0;
         assert decoded < 65566;
-        final StringBuilder builder = new StringBuilder();
-        builder.ensureCapacity(builder.capacity() + I_MAX_VALUE_DIGITS);
-        builder.append(decoded);
-        int p;
-        for (p = 0; builder.length() < S_MAX_VALUE_DIGITS; p++) {
-            builder.append(current().nextInt(10)); // 0 - 9
+        int v = 0;
+        for (int i = 0; i < 4; i++) {
+            v <<= 5;
+            v |= 0b10000 | (decoded & 0b1111);
+            decoded >>= 4;
         }
-        assert p >= 0;
-        assert p < S_MAX_VALUE_DIGITS;
-        builder.append(format("%03d", current().nextInt(474)));
-        builder.append(p);
-        builder.append(p >= 1 ? 1 : current().nextInt(1, 3));
-        builder.reverse();
-        final String encoded = Integer.toString(parseInt(builder.toString()), MAX_RADIX);
-        assert encoded.length() == 6;
-        return encoded;
+        return Integer.toString(v, MAX_RADIX);
     }
 
-    static int decode16(final String encoded) {
+    static int decodeInt4(final String encoded) {
         assert encoded != null;
-        assert encoded.length() == 6;
-        final StringBuilder builder = new StringBuilder();
-        builder.append(parseInt(encoded, MAX_RADIX));
-        builder.delete(0, 1);
-        final int p = getNumericValue(builder.charAt(0));
-        builder.delete(0, p + (1 + 3));
-        builder.reverse();
-        final int decoded = parseInt(builder.toString());
-        assert decoded >= 0;
-        assert decoded < 65536;
+        assert encoded.length() == 4;
+        int decoded = 0;
+        int v = parseInt(encoded, MAX_RADIX);
+        for (int i = 0; i < 4; i++) {
+            decoded <<= 4;
+            decoded |= v & 0b1111;
+            v >>= 5;
+        }
         return decoded;
     }
 
-    static <R> R encodeInt(final int decoded, final BiFunction<? super String, ? super String, ? extends R> function) {
-        requireNonNull(function, "function is null");
-        return function.apply(encode16(decoded >>> Short.SIZE), encode16(decoded & 0xFFFF));
+    static String encodeInt8(final int decoded) {
+        return encodeInt4(decoded >>> Short.SIZE).concat(encodeInt4(decoded & 0xFFFF));
     }
 
-    static String encodeInt(final int decoded) {
-        return encodeInt(decoded, (s1, s2) -> s1 + "-" + s2);
+    static int decodeInt8(final String encoded) {
+        if (requireNonNull(encoded, "encoded is null").length() != 8) {
+            throw new IllegalArgumentException("encoded.length(" + encoded.length() + ") != 8");
+        }
+        return decodeInt4(encoded.substring(0, 4)) << Short.SIZE | decodeInt4(encoded.substring(4));
     }
 
-    static int decodeInt(final String h, final String l) {
-        // TODO: 2020/05/29 validate arguments!
-        return (decode16(h) << Short.SIZE) | decode16(l);
+    static String encodeInt7(long decoded) {
+        long v = 0;
+        for (int i = 0; i < 3; i++) {
+            v <<= 5;
+            v |= 0b10000 | (decoded & 0b1111);
+            decoded >>= 4;
+        }
+        for (int i = 0; i < 4; i++) {
+            v <<= 5;
+            v |= decoded & 0b11111;
+            decoded >>= 5;
+        }
+        final String encoded = Long.toString(v, MAX_RADIX);
+        assert encoded.length() == 7;
+        return encoded;
     }
 
-    static int decodeInt(final String encoded) {
-        final int index = encoded.indexOf('-');
-        assert index != -1;
-        return decodeInt(encoded.substring(0, index), encoded.substring(index + 1));
+    static long decodeInt7(final String encoded) {
+        if (requireNonNull(encoded, "encoded is null").length() != 7) {
+            throw new IllegalArgumentException("encoded.length(" + encoded.length() + ") != 7");
+        }
+        long decoded = 0;
+        long v = parseLong(encoded, MAX_RADIX);
+        for (int i = 0; i < 4; i++) {
+            decoded <<= 5;
+            decoded |= v & 0b11111;
+            v >>= 5;
+        }
+        for (int i = 0; i < 3; i++) {
+            decoded <<= 4;
+            decoded |= v & 0b1111;
+            v >>= 5;
+        }
+        return decoded;
     }
 }
--- src/test/java/com/github/jinahya/codec/CouponNumberTest.java
+++ src/test/java/com/github/jinahya/codec/CouponNumberTest.java
@@ -1,59 +1,80 @@
 package com.github.jinahya.codec;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
-import static com.github.jinahya.codec.CouponNumber.decode16;
-import static com.github.jinahya.codec.CouponNumber.decodeInt;
-import static com.github.jinahya.codec.CouponNumber.encode16;
-import static com.github.jinahya.codec.CouponNumber.encodeInt;
+import static com.github.jinahya.codec.CouponNumber.*;
+import static java.lang.String.format;
 import static java.util.concurrent.ThreadLocalRandom.current;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class CouponNumberTest {
 
     @Test
     void encodeDecodeInt16() {
-        for (int expected = 0; expected < 65536; expected++) {
-            final String encoded = encode16(expected);
-            final int actual = decode16(encoded);
-            assertEquals(expected, actual);
-        }
-    }
-
-    @RepeatedTest(8192)
-    void encodeDecodeInt() {
-        final int expected = current().nextInt();
-        final String encoded = encodeInt(expected);
-        final int actual = decodeInt(encoded);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void encodeDecodeInt_duplicate() {
         final Set<String> set = new HashSet<>();
-        for (int i = 0; i < 8192; i++) {
-            final int expected = current().nextInt();
-            final String encoded = encodeInt(expected);
+        for (int expected = 0; expected < 65536; expected++) {
+            final String encoded = encodeInt4(expected);
             assertTrue(set.add(encoded));
-            final int actual = decodeInt(encoded);
+            final int actual = decodeInt4(encoded);
             assertEquals(expected, actual);
         }
     }
 
     @Test
-    void encodeDecodeInt_debug() {
-        final int expected = current().nextInt();
-        log.debug("expected: {}", expected);
-        final String encoded = encodeInt(expected);
-        log.debug("encoded: {}", encoded);
-        final int actual = decodeInt(encoded);
-        assertEquals(expected, actual);
+    void encodeDecodeInt0() {
+        IntStream.range(0, 128).forEach(expected -> {
+            final String encoded = encodeInt8(expected);
+            final int actual = decodeInt8(encoded);
+            assertEquals(expected, actual);
+            log.debug("int {} -> {}", expected, encoded);
+        });
+    }
+
+    @Test
+    void encodeDecodeIntRandom() {
+        final Set<Integer> ds = new HashSet<>();
+        final Set<String> es = new HashSet<>();
+        for (int i = 0; i < 1024; i++) {
+            final int expected = current().nextInt();
+            final String encoded = encodeInt8(expected);
+            log.debug("int {} -> {}", format("%08x", expected), encoded);
+            assertSame(ds.add(expected), es.add(encoded));
+            final int actual = decodeInt8(encoded);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void encodeDecodeInt() {
+        final Set<Integer> ds = new HashSet<>();
+        final Set<String> es = new HashSet<>();
+        for (int i = 0; i < 1048576; i++) {
+            final int expected = current().nextInt();
+            final String encoded = encodeInt8(expected);
+//            log.debug("int {} -> {}", expected, encoded);
+            assertSame(ds.add(expected), es.add(encoded));
+            final int actual = decodeInt8(encoded);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void encodeDecodeInt7() {
+        final Set<Long> ds = new HashSet<>();
+        final Set<String> es = new HashSet<>();
+        for (int i = 0; i < 1; i++) {
+            final long expected = current().nextLong();
+            final String encoded = encodeInt7(expected);
+            log.debug("int {} -> {}", expected, encoded);
+            assertSame(ds.add(expected), es.add(encoded));
+            final long actual = decodeInt7(encoded);
+            assertEquals(expected, actual);
+        }
     }
 }
